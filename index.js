025b97d940882f6274ac2a63e4e088e286b2779a
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-03 17:23:48 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+// Imports
 const express = require('express');
-const logger = require('pino');
-const path = require('path');
 
+//const path = require('path');
+const logger = require('pino');
+
+// Express app variable
 const app = express();
 
-app.set.views(path.join(__dirname, 'views'));
+// Set views
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'ejs');
+// Use ejs as the view engine
+app.set("view engine", "ejs");
 
-const port = process.env.PORT || 3000;
+// Set static files, folder: public, has images and stylesheets
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/site", express.static(path.join(__dirname, "public/stylesheets")));
 
+// Holds server port value
+const PORT = process.env.PORT || 3000;
+
+// Calling Landing Page
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Home',
+        message: 'Welcome to the Home Page!'
+    })
 });
+
+//Listens on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`));
