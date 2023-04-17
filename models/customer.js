3a817e9cf2e7f290c78257ab01aa54c3622af695
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-17 18:54:01 -0500

    updating schema

--- models/customer.js
+++ models/customer.js
@@ -4,8 +4,14 @@ const Schema = mongoose.Schema;
 
 // Creating a new mongoose model named Customer with properties for customerId and email
 let customerSchema = new Schema({
-    customerId: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true }
+    customerId: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    streetAddress: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    zipCode: {type: String, required: true},
+    email: {type: String, required: true, unique: true}
 });
 
 // Exporting the model
